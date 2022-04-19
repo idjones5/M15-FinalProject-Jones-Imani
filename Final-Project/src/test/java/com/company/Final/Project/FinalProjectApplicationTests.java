185a677cff117e378568345e6e2cf15a13248d66
idjones5 <97227301+idjones5@users.noreply.github.com>
2022-04-19 15:34:12 -0700

    Initial commit

---
+++ Final-Project/src/test/java/com/company/Final/Project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Final.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
