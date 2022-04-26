cfbd1e1289ed4c10273d7b7c48fa9ce06d886940
idjones5 <97227301+idjones5@users.noreply.github.com>
2022-04-25 20:17:11 -0700

    Removal of unecessary comments in Coord class.

--- Final-Project/src/main/java/com/company/Final/Project/weather/Coord.java
+++ Final-Project/src/main/java/com/company/Final/Project/weather/Coord.java
@@ -2,11 +2,9 @@ package com.company.Final.Project.weather;
 
 public class Coord {
 
-    // attributes
     private String lon;
     private String lat;
-
-    // getters and setters
+    
     public String getLon() {
         return lon;
     }
